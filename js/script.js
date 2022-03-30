46722f3b51f5aa53ea8f404b5efc4842c45e50eb
jguerreiro <joel.borghi@gmail.com>
2022-03-30 18:46:22 -0300

    update - refatorando

--- js/script.js
+++ js/script.js
@@ -1,37 +1,52 @@
 function adicionarEventos() {
 
-    const botoesTeclado = document.querySelector( ".conteiner-teclado" );
-    const display = document.querySelector( ".conteiner-display");
-    const botao = document.querySelector(".conteiner-botao");
+  const botoesTeclado = document.querySelector(".conteiner-teclado");
+  const display = document.querySelector(".conteiner-display");
+  const botao = document.querySelector(".conteiner-botao");
 
-    const mostrarValores = (event) => {
 
-        const valor = event.target;
-        const valorBotao = valor.textContent;
-        const valorDisplay = display.textContent;
-        display.textContent = valorBotao;
 
-        valorDisplay == 0 ? display.textContent = valorBotao : display.textContent = valorDisplay + valorBotao;
-        
-    };
+  const mostrarValores = (event) => {
 
-    botoesTeclado.addEventListener('click', mostrarValores );
+    const valor = event.target;
+    const valorBotao = valor.textContent;
+    const valorDisplay = display.textContent;
+    display.textContent = valorBotao;
+    valorDisplay == 0 ? display.textContent = valorBotao : display.textContent = valorDisplay + valorBotao;
+
+  };
+
+
+
+  botoesTeclado.addEventListener('click', mostrarValores);
+  // botoesTeclado.addEventListener('click', limparVisor);
+
 }
-window.addEventListener('load', adicionarEventos );
-
-/*
+window.addEventListener('load', adicionarEventos);
 
 // CLEAR
-function limparVisor () {
-    //limpa o visor
+
+const limparVisor = () => {
+  //limpa o visor
+  window.location.reload();
 }
+
+
+// +
+const somar = (num1, num2) => {
+  //recebe dois valores, subtrai o primeiro pelo segundo e retorna o resultado
+  var resultado = num1 + num2;
+  return resultado;
+}
+
+/*
 
 // ÷
 function dividir( num1, num2 ) {
     //recebe dois valores divide o primeiro pelo segundo e retorna o resultado
     var resultado = num1 / num2 ;
     return resultado;
-    
+
 }
 
 // X
@@ -48,12 +63,6 @@ function subtrair( num1, num2 ) {
     return resultado;
 }
 
-// +
-function somar( num1, num2 ) {
-    //recebe dois valores, subtrai o primeiro pelo segundo e retorna o resultado
-    var resultado = num1 + num2 ;
-    return resultado;
-}
 
 // =
 function igual( valor ) {
